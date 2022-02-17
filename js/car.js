042b6ae72a8d03ab2ce35246e51a0237e96a3db4
Orintaa <orinta.strac@gmail.com>
2022-02-17 21:11:34 +0200

    math-floor

--- js/car.js
+++ js/car.js
@@ -8,9 +8,9 @@
  * @returns Sakinys
  */
 function car(carModel, fullTank, costsInHighway, costsOnAverage, costsInCity) {
-    const distanceHighway = Math.round(fullTank /costsInHighway * 100);
-    const distanceAverange = Math.round(fullTank /costsOnAverage * 100);
-    const distanceCity = math.round(fullTank /costsInCity * 100);
+    const distanceHighway = Math.floor(fullTank /costsInHighway * 100);
+    const distanceAverange = Math.floor(fullTank /costsOnAverage * 100);
+    const distanceCity = Math.floor(fullTank /costsInCity * 100);
 
     return `${carModel} su ${fullTank}l baku, gali nuvaziuoti ${distanceHighway}km (magistralej, kai sanaudos yra ${costsInHighway}l/100km), ${distanceAverange}km (vidutiniskai, kai sanaudos yra ${costsOnAverage}l/100km) ir ${distanceCity}km (mieste, kai sanaudos yra ${costsInCity}l/100km)`;
 }
